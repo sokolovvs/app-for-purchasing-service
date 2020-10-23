767b547c039d15fbd603d48283ffb7c558785823
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2020-10-24 02:22:53 +0600

    add implementation for application transaction interface

---
+++ src/Components/TransactionScripts/RelationalDbTransaction.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Components\TransactionScripts;
+
+
+use Doctrine\ORM\EntityManagerInterface;
+
+class RelationalDbTransaction implements ApplicationTransactionInterface
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function beginTransaction(): void
+    {
+        $this->entityManager->beginTransaction();
+    }
+
+    public function commit(): void
+    {
+        $this->entityManager->commit();
+    }
+
+    public function rollback(): void
+    {
+        $this->entityManager->rollback();
+    }
+}
