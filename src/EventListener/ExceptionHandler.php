b0ce9315f9486d5274215c94d4975f49b6473ac7
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2020-10-24 00:45:51 +0600

    register global exception handler

---
+++ src/EventListener/ExceptionHandler.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace App\EventListener;
+
+
+use App\Components\Errors\HttpError;
+use App\Components\Exceptions\DomainExceptions\ImproveDomainException;
+use App\Components\Serializers\Normalizers\HttpError\HttpErrorNormalizerInterface;
+use App\Components\Transformers\Exceptions\ExceptionToHttpErrorTransformerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ExceptionHandler implements EventSubscriberInterface
+{
+    private HttpErrorNormalizerInterface $httpErrorNormalizer;
+    private LoggerInterface $logger;
+    private ExceptionToHttpErrorTransformerInterface $exceptionToHttpErrorTransformer;
+
+    public function __construct(
+        HttpErrorNormalizerInterface $httpErrorNormalizer,
+        ExceptionToHttpErrorTransformerInterface $exceptionToHttpErrorTransformer,
+        LoggerInterface $logger
+    ) {
+        $this->httpErrorNormalizer = $httpErrorNormalizer;
+        $this->logger = $logger;
+        $this->exceptionToHttpErrorTransformer = $exceptionToHttpErrorTransformer;
+    }
+
+    public function handle(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+        $httpError = $this->exceptionToHttpErrorTransformer->transform($exception);
+        $this->logger->warning('Exception was handled in global exception handler', ['exception' => $exception]);
+
+        if (!$exception instanceof ImproveDomainException || !$exception instanceof NotFoundHttpException) {
+            $this->logger->critical('Unknown error', ['exception' => $exception]);
+        }
+
+        $this->sendResponse($event, $httpError);
+    }
+
+    private function sendResponse(ExceptionEvent $event, HttpError $httpError): void
+    {
+        //TODO: need check accept header and send response with needs format
+        $event->setResponse(
+            new JsonResponse(
+                $this->httpErrorNormalizer->normalize($httpError), $httpError->getStatus(),
+                ['Content-Type' => 'application/problem+json']
+            )
+        );
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::EXCEPTION => 'handle',
+        ];
+    }
+}
