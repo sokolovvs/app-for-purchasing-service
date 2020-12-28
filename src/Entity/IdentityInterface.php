31aedfaf9ff00d48d6aee4d0490a9efcef931e0f
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2020-12-28 12:45:42 +0600

    added typehint for IdentityInterface

--- src/Entity/IdentityInterface.php
+++ src/Entity/IdentityInterface.php
@@ -4,10 +4,12 @@
 namespace App\Entity;
 
 
+use Ramsey\Uuid\UuidInterface;
+
 interface IdentityInterface
 {
     /**
-     * @return int|string|null
+     * @return int|UuidInterface|null
      */
     public function getId();
 }
