f69a0b043a32e04e962ca9a55f77edc9a5edd4eb
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2020-12-28 12:46:08 +0600

    implement method get customer by id

--- src/Repository/User/CustomerRepository.php
+++ src/Repository/User/CustomerRepository.php
@@ -4,14 +4,31 @@
 namespace App\Repository\User;
 
 
+use App\Components\Exceptions\ApplicationExceptions\Resource\ResourceNotFoundException;
 use App\Entity\User\Customer;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
+/**
+ * Class CustomerRepository
+ *
+ * @package App\Repository\User
+ */
 class CustomerRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Customer::class);
+    }
+
+    public function getById($id): Customer
+    {
+        $customer = $this->find($id);
+
+        if ($customer !== null) {
+            return $customer;
+        }
+
+        throw new ResourceNotFoundException('Customer not found', null, ['id' => $id]);
     }
 }
