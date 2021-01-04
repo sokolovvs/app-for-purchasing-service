ead4d3d1e5a42b6dcaabb8a529571ec2d8d6894c
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-01-04 15:44:07 +0600

    login functional was implemented

--- src/Components/Interactors/Auth/AuthManager.php
+++ src/Components/Interactors/Auth/AuthManager.php
@@ -4,36 +4,85 @@
 namespace App\Components\Interactors\Auth;
 
 
+use App\Components\Exceptions\ApplicationExceptions\Resource\ResourceNotFoundException;
 use App\Components\Exceptions\ApplicationExceptions\Security\UnauthorizedException;
 use App\Entity\User\User;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use App\Repository\User\UserRepository;
+use Firebase\JWT\JWT;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class AuthManager
 {
-    private $tokenStorage;
+    private UserRepository $userRepository;
+    private LoggerInterface $logger;
+    private TokenIssuer $issuer;
 
-    public function __construct(
-        TokenStorageInterface $storage,
-    ) {
-        $this->tokenStorage = $storage;
+    public function __construct(UserRepository $userRepository, TokenIssuer $issuer, LoggerInterface $logger)
+    {
+        $this->userRepository = $userRepository;
+        $this->logger = $logger;
+        $this->issuer = $issuer;
     }
 
-    public function getCurrentUser(): ?User
+    public function getCurrentUser(Request $request): ?User
     {
-        $token = $this->tokenStorage->getToken();
-
-        if ($token instanceof TokenStorageInterface) {
-            /** @var User $user */
-            $user = $token->getUser();
-
-            return $user;
+        try {
+            return $this->extractUserFromDecodedToken($this->decodeToken($request->headers->get('Authorization')));
+        } catch (\Throwable $throwable) {
+            $this->logger->info(sprintf("%s%s%s", $throwable->getMessage(), PHP_EOL, $throwable->getTraceAsString()));
         }
 
         return null;
     }
 
-    public function getCurrentUserOrThrowException(): User
+    public function getCurrentUserOrThrowException(Request $request): User
     {
-        return $this->getCurrentUser() ?? throw new UnauthorizedException();
+        return $this->getCurrentUser($request) ?? throw new UnauthorizedException();
+    }
+
+    public function issueTokenForUser(User $user): string
+    {
+        $payload['user_id'] = $user->getId();
+
+        return $this->issuer->fromPayload($payload);
+    }
+
+    /**
+     * @param string $email
+     * @param string $password
+     *
+     * @return string
+     * @throws UnauthorizedException
+     */
+    public function signIn(string $email, string $password): string
+    {
+        try {
+            $user = $this->userRepository->findActiveByEmail($email);
+
+            if ($user === null) {
+                throw new ResourceNotFoundException();
+            }
+
+            if (!password_verify($password, $user->getPasswordHash())) {
+                throw UnauthorizedException::invalidCredentials();
+            }
+
+            return $this->issuer->fromUser($user);
+        } catch (\Throwable) {
+            throw UnauthorizedException::invalidCredentials();
+        }
+    }
+
+    private function decodeToken(string $token): array
+    {
+        $token = explode(' ', $token)[1];
+
+        return (array)JWT::decode($token, file_get_contents('../config/jwt/public.pem'), ['RS256']);
+    }
+
+    private function extractUserFromDecodedToken(array $tokenPayload): ?User
+    {
+        return $this->userRepository->findById($tokenPayload['user_id'] ?? null);
     }
 }
---
+++ src/Controller/User/Login/LoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Controller\User\Login;
+
+
+use App\Components\Exceptions\ApplicationExceptions\Security\UnauthorizedException;
+use App\Components\Interactors\Auth\AuthManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LoginController extends AbstractController
+{
+    private AuthManager $authManager;
+
+    public function __construct(AuthManager $authManager)
+    {
+        $this->authManager = $authManager;
+    }
+
+    #[Route('/api/v1/sign-in', name: 'sign-in', methods: ['POST'])]
+    public function confirmEmail(
+        Request $request
+    ): JsonResponse {
+        try {
+            return $this->json(
+                $this->authManager->signIn($request->request->get('email'), $request->request->get('password'))
+            );
+        } catch (\Throwable) {
+            throw UnauthorizedException::invalidCredentials();
+        }
+    }
+}
