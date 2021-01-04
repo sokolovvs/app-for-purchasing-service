970288b4b8b4a1ff41a4c851050dda141b5f1123
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-01-04 15:44:25 +0600

    added param uuid to AddEmailToConfirmDto

--- src/Components/Dto/EmailConfirm/AddEmailToConfirmDto.php
+++ src/Components/Dto/EmailConfirm/AddEmailToConfirmDto.php
@@ -5,31 +5,33 @@ namespace App\Components\Dto\EmailConfirm;
 
 
 use App\Entity\User\User;
+use Ramsey\Uuid\UuidInterface;
 
 final class AddEmailToConfirmDto
 {
     private string $hash;
     private User $user;
+    private UuidInterface $uuid;
 
-    public function __construct(string $hash, User $user)
+    public function __construct(UuidInterface $uuid, string $hash, User $user)
     {
         $this->hash = $hash;
         $this->user = $user;
+        $this->uuid = $uuid;
     }
 
-    /**
-     * @return string
-     */
     public function getHash(): string
     {
         return $this->hash;
     }
 
-    /**
-     * @return User
-     */
     public function getUser(): User
     {
         return $this->user;
+    }
+
+    public function getUuid(): UuidInterface
+    {
+        return $this->uuid;
     }
 }
